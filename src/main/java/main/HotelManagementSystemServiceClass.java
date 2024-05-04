7efb53112b757ea38a022a8f436fd83ffc4580f4
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-04 19:48:46 +0300

    HotelManagementServiceClass Options and methods

--- src/main/java/main/HotelManagementSystemServiceClass.java
+++ src/main/java/main/HotelManagementSystemServiceClass.java
@@ -13,7 +13,7 @@ public class HotelManagementSystemServiceClass {
         scanner = new Scanner(System.in);
 
         //menu loop
-        boolean exit = true;
+        boolean exit = false;
 
         while (!exit) {
             System.out.println("======= Hotel Management System Menu =======");
@@ -23,7 +23,31 @@ public class HotelManagementSystemServiceClass {
             System.out.println("4. Reservation Operation");
             System.out.println("5. Exit Operation");
 
-            System.out.println("Enter you Option:");
+            System.out.println("Enter you Selection Option Number:");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // it'll consume NewLine
+
+            switch (choice) {
+                case 1:
+                    System.out.println("1. Display Hotel Operation");
+                    break;
+                case 2:
+                    System.out.println("2. Display Room Operation");
+                    break;
+                case 3:
+                    System.out.println("3. Display Guest Operation");
+                    break;
+                case 4:
+                    System.out.println("4. Display Reservation Operation");
+                    break;
+                case 5:
+                    System.out.println("5. Display Exit Operation");
+                    break;
+                default:
+                    System.out.println("Invalid Choice. Please Try Again Later!");
+                    break;
+            }
         }
 
     }
