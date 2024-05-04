f0f9eba69cd2ed0989c6be202543eefe93cd3bc4
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-04 19:25:49 +0300

    Configuring Hibernate Class Configuration

---
+++ src/main/java/exception/HotelNotFoundException.java
@@ -0,0 +1,10 @@
+package exception;
+
+//step 8: Custom Exception
+public class HotelNotFoundException extends RuntimeException {
+
+    public HotelNotFoundException(String message) {
+        super(message);
+    }
+
+}
