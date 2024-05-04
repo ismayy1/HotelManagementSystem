3f857a09b438a9c12fe63e48fb1607bd2e637b75
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-04 20:11:43 +0300

    Hotel Entity Set Up

--- src/main/java/model/Room.java
+++ src/main/java/model/Room.java
@@ -1,4 +1,28 @@
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "tbl_room")
 public class Room {
+
+    @Id
+    private Long id;
+    @Column (nullable = false)
+    private String number;
+    @Column (nullable = false)
+    private int capacity;
+
+    public Room() {}
+
+    public Room(Long id, String number, int capacity) {
+        this.id = id;
+        this.number = number;
+        this.capacity = capacity;
+    }
+
+
 }
