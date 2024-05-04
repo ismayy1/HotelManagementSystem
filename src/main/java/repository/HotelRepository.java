d9a5f7148295cfabc9d6b7dc895d6aa78955d1e8
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-04 20:45:56 +0300

    Repositories

---
+++ src/main/java/repository/HotelRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+import model.Hotel;
+
+// step 13 a: Create Hotel
+public interface HotelRepository {
+
+    Hotel saveHotel (Hotel hotel);
+
+}
---
+++ src/main/java/repository/HotelRepositoryImplement.java
@@ -0,0 +1,13 @@
+package repository;
+
+import model.Hotel;
+
+// step 13 b: override method
+public class HotelRepositoryImplement implements HotelRepository {
+
+    @Override
+    public Hotel saveHotel(Hotel hotel) {
+        return null;
+    }
+
+}
