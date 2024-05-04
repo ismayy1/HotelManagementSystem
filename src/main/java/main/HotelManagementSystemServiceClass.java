f6c472e57e6677d997b5efc1c2f95a628f1b911a
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-04 19:41:27 +0300

    HotelManagementServiceClass Options and methods

--- src/main/java/main/HotelManagementSystemServiceClass.java
+++ src/main/java/main/HotelManagementSystemServiceClass.java
@@ -1,4 +1,31 @@
 package main;
 
+import java.util.Scanner;
+
+//step 10: Create HMS Service Class we need to Call this class in Main Class
 public class HotelManagementSystemServiceClass {
+
+    private static Scanner scanner;
+
+    public static void displayMenuHotelManagementSystem() {
+
+        // create Scanner for the user input
+        scanner = new Scanner(System.in);
+
+        //menu loop
+        boolean exit = true;
+
+        while (!exit) {
+            System.out.println("======= Hotel Management System Menu =======");
+            System.out.println("1. Hotel Operation");
+            System.out.println("2. Room Operation");
+            System.out.println("3. Guest Operation");
+            System.out.println("4. Reservation Operation");
+            System.out.println("5. Exit Operation");
+
+            System.out.println("Enter you Option:");
+        }
+
+    }
+
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,4 +1,11 @@
 package main;
 
 public class Main {
+    public static void main(String[] args) {
+
+        // step 11: Call HMS Service Class to display the Menu
+
+
+
+    }
 }
