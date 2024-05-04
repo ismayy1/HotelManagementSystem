1eee32a619daefad01dda57668a2fa41f4dc3b69
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-04 20:07:06 +0300

    Hotel Entity Set Up

--- src/main/java/model/Hotel.java
+++ src/main/java/model/Hotel.java
@@ -1,5 +1,54 @@
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 //step 9: Hotel Entity and Room
+@Entity
+@Table (name = "tbl_hotel")
 public class Hotel {
+
+    // step 12: we can add field about Hotel
+    @Id
+    private Long id;
+    @Column (nullable = false)
+    private String name;
+    @Column (nullable = false, length = 44)
+    private String location;
+
+    public Hotel() {}
+
+    public Hotel(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    @Override
+    public String toString() {
+        return "Hotel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                '}';
+    }
 }
