7410177618bfe7ec140556506d66d0ffa1f9f0a8
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-13 20:31:35 +0300

    Practice session Day 3, ReoomService Operations

--- src/main/java/com/tpe/model/Guest.java
+++ src/main/java/com/tpe/model/Guest.java
@@ -26,4 +26,37 @@ public class Guest {
         createDate = LocalDateTime.now();
     }
 
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Address getAddress() {
+        return address;
+    }
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+    @Override
+    public String toString() {
+        return "Guest{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+//                ", address=" + address +
+                ", createDate=" + createDate +
+                '}';
+    }
 }
