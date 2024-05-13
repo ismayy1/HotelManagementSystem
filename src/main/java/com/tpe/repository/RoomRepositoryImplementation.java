2def21118f56c964cdc5ef346607d5511826b363
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-13 19:02:54 +0300

    Practice session Day 3, Room oprations

--- src/main/java/com/tpe/repository/RoomRepositoryImplementation.java
+++ src/main/java/com/tpe/repository/RoomRepositoryImplementation.java
@@ -1,13 +1,27 @@
 package com.tpe.repository;
 
+import com.tpe.config.HibernateUtils;
 import com.tpe.model.Room;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class RoomRepositoryImplementation implements RoomRepository {
 
     // step 18 b: save Room
     @Override
     public Room saveRoom(Room room) {
-        return null;
+
+        try {
+            Session session = HibernateUtils.getSessionFactory().openSession();
+            Transaction transaction = session.beginTransaction();
+
+            session.save(room);
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return room;
     }
 
 }
