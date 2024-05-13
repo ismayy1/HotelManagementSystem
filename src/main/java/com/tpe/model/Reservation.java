fd84905e0f8229c2cb5b6bc18388bfc10c2ad495
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-13 20:38:26 +0300

    Practice session Day 3, ReoomService Operations

--- src/main/java/com/tpe/model/Reservation.java
+++ src/main/java/com/tpe/model/Reservation.java
@@ -1,4 +1,23 @@
 package com.tpe.model;
 
+import javax.persistence.*;
+
+// step 9 d: create reservation
+@Entity
+@Table (name = "tbl_reservation")
 public class Reservation {
+
+    // step 26: Reservation file here
+
+    @Id
+    @GeneratedValue (generator = "sequence", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(
+            name = "sequence",
+            sequenceName = "reservation_id",
+            initialValue = 4001
+    )
+    private Long id;
+
+
+
 }
