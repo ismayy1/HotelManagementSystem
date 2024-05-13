579e6eca86ea2817ed1687141aa1d949d15c1e16
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-13 21:17:00 +0300

    Practice session Day 3, ReoomService Operations

--- src/main/java/com/tpe/service/GuestServiceImplementation.java
+++ src/main/java/com/tpe/service/GuestServiceImplementation.java
@@ -1,5 +1,6 @@
 package com.tpe.service;
 
+import com.tpe.model.Address;
 import com.tpe.model.Guest;
 import com.tpe.repository.GuestRepository;
 
@@ -23,8 +24,29 @@ public class GuestServiceImplementation implements GuestService {
 
         Guest guest = new Guest();
 
+        System.out.println("Please enter the guest name: ");
+        guest.setName(scanner.nextLine());
 
+        // create Address object
+        Address address = new Address();
 
-        return null;
+        System.out.println("Enter the street: ");
+        address.setAddress(scanner.nextLine());
+
+        System.out.println("Enter the country: ");
+        address.setCountry(scanner.nextLine());
+
+        System.out.println("Enter the zipCode: ");
+        address.setZipCode(scanner.nextLine());
+
+        // set the address for the guest
+        guest.setAddress(address);
+
+        // save the guest using the guest repository
+        guestRepository.saveGuest(guest);
+
+        System.out.println("Guest Saved successfully with and id: " + guest.getId());
+
+        return guest;
     }
 }
