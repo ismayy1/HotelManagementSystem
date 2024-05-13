5dcd497244a577d288aa2b8ac1f231fe73e7994b
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-13 19:00:58 +0300

    Practice session Day 3, Room oprations

--- src/main/java/com/tpe/repository/RoomRepository.java
+++ src/main/java/com/tpe/repository/RoomRepository.java
@@ -1,4 +1,10 @@
 package com.tpe.repository;
 
+import com.tpe.model.Room;
+
 public interface RoomRepository {
+
+    // step 18 a: save Room
+    Room saveRoom(Room room);
+
 }
--- src/main/java/com/tpe/repository/RoomRepositoryImplementation.java
+++ src/main/java/com/tpe/repository/RoomRepositoryImplementation.java
@@ -1,4 +1,13 @@
 package com.tpe.repository;
 
+import com.tpe.model.Room;
+
 public class RoomRepositoryImplementation implements RoomRepository {
+
+    // step 18 b: save Room
+    @Override
+    public Room saveRoom(Room room) {
+        return null;
+    }
+
 }
