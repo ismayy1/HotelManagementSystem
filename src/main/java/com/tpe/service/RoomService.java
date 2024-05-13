d4fbb28d50ca6ef1cf5b6cd6b5541d467acdb4ff
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-13 20:07:58 +0300

    Practice session Day 3, ReoomService Operations

--- src/main/java/com/tpe/service/RoomService.java
+++ src/main/java/com/tpe/service/RoomService.java
@@ -2,9 +2,20 @@ package com.tpe.service;
 
 import com.tpe.model.Room;
 
+import java.util.List;
+
 public interface RoomService {
 
     // step 18 c: save Room
     Room saveRoom();
+
+    //step 19c :findRoomById
+    Room findRoomById(Long id);
+
+    //step 20c: findAllRoom
+    List<Room> findAllRooms();
+
+    //step 21c:deleteRoomById
+    void deleteRoomById(Long id);
 
 }
--- src/main/java/com/tpe/service/RoomServiceImplementation.java
+++ src/main/java/com/tpe/service/RoomServiceImplementation.java
@@ -1,11 +1,14 @@
 package com.tpe.service;
 
 import com.tpe.exception.HotelNotFoundException;
+import com.tpe.exception.RoomNotFoundException;
 import com.tpe.model.Hotel;
 import com.tpe.model.Room;
 import com.tpe.repository.HotelRepository;
 import com.tpe.repository.RoomRepository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class RoomServiceImplementation implements RoomService {
@@ -64,7 +67,64 @@ public class RoomServiceImplementation implements RoomService {
         } catch (HotelNotFoundException e) {
             e.printStackTrace();
         }
-
         return room;
+    }
+
+
+
+    // Step 19d :findRoomById
+    @Override
+    public Room findRoomById(Long id) {
+        try {
+            Room foundRoom = roomRepository.findRoomById(id);
+            if (foundRoom != null) {
+                System.out.println("---------------------------------");
+                System.out.println(foundRoom);
+
+                return foundRoom;
+            } else {
+                throw new RoomNotFoundException(" Room  not found with ID: " + id);
+            }
+        }catch (RoomNotFoundException e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    //step 20c :findAllRooms
+
+    @Override
+    public List<Room> findAllRooms() {
+        try {
+            List<Room> rooms = roomRepository.findAllRoom();
+            if (!rooms.isEmpty()) {
+                for (Room room : rooms) {
+                    System.out.println(room);
+                }
+            } else {
+                throw new RuntimeException("No rooms found.");
+            }
+            return rooms;
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            return new ArrayList<>(); // Return an empty ArrayList
+        }
+    }
+
+
+    //step 21d : deleteRoomById
+    @Override
+    public void deleteRoomById(Long id) {
+        try {
+            Room existingRoom = roomRepository.findRoomById(id);
+            if (existingRoom == null) {
+                throw new RoomNotFoundException("Room not found with ID: " + id);
+            }
+
+            roomRepository.deleteRoomById(id);
+            System.out.println("Room  deleted successfully. ID: " + id);
+        } catch (RoomNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
