6f7a2c5761809d08c08bc343095a2ac99273cbd1
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-13 20:24:04 +0300

    Practice session Day 3, ReoomService Operations

--- src/main/java/com/tpe/model/Address.java
+++ src/main/java/com/tpe/model/Address.java
@@ -1,4 +1,50 @@
 package com.tpe.model;
 
+
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Address {
+
+    private String address;
+
+    private String city;
+
+    private String country;
+
+    private String zipCode;
+
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public String getZipCode() {
+        return zipCode;
+    }
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+    @Override
+    public String toString() {
+        return "Address{" +
+                "address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/tpe/model/Guest.java
+++ src/main/java/com/tpe/model/Guest.java
@@ -1,5 +1,17 @@
 package com.tpe.model;
 
+import javax.persistence.*;
+
 //step 9: Create guest Entity
+@Entity
+@Table (name = "tbl_guest")
 public class Guest {
+
+    // step 22 fild the guest
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
 }
