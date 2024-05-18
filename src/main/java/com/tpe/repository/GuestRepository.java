8e18c4a038b3318964e497289b3c72b8d05239d2
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-18 21:16:45 +0300

    Practice session, Guest Implementations

--- src/main/java/com/tpe/repository/GuestRepository.java
+++ src/main/java/com/tpe/repository/GuestRepository.java
@@ -2,9 +2,21 @@ package com.tpe.repository;
 
 import com.tpe.model.Guest;
 
+import java.util.List;
+
 public interface GuestRepository {
 
     // step 22 a: save guest
     void saveGuest(Guest guest);
+
+    //23a : findGuestById
+
+    Guest findGuestById(Long guestId);
+
+    //24a: deleteGuestById
+    void deleteGuestById(Long id);
+
+    //25a: findALlGuest
+    List<Guest> findAllGuest();
 
 }
--- src/main/java/com/tpe/repository/GuestRepositoryImplementation.java
+++ src/main/java/com/tpe/repository/GuestRepositoryImplementation.java
@@ -1,10 +1,16 @@
 package com.tpe.repository;
 
 import com.tpe.config.HibernateUtils;
+import com.tpe.exception.GuestNotFoundException;
 import com.tpe.model.Address;
 import com.tpe.model.Guest;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
 public class GuestRepositoryImplementation implements GuestRepository {
 
@@ -35,5 +41,68 @@ public class GuestRepositoryImplementation implements GuestRepository {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    //23b : findGuestById
+//    @Override
+//    public Guest findGuestById(Long guestId) {
+//        Session session = HibernateUtil.getSessionFactory().openSession();
+//        return session.get(Guest.class, guestId);
+//    }
+
+
+    // Criteria API findGuestByID:
+    @Override
+    public Guest findGuestById(Long guestId) {
+        try (Session session = HibernateUtils.getSessionFactory().openSession()) {
+
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<Guest> query = builder.createQuery(Guest.class);
+            Root<Guest> root = query.from(Guest.class);
+
+            query.select(root).where(builder.equal(root.get("id"), guestId));
+
+            return session.createQuery(query).uniqueResult();
+        } catch (GuestNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+
+    //24b: deleteGuestById
+    @Override
+    public void deleteGuestById(Long id) {
+        try (Session session = HibernateUtils.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+
+            Guest guest = session.get(Guest.class, id);
+            if (guest != null) {
+                // Remove the guest from any associated reservations
+                guest.getReservations().forEach(reservation -> reservation.setGuest(null));
+
+                // Delete the guest
+                session.delete(guest);
+            }
+
+
+
+            transaction.commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    //25a: findALlGuest
+    @Override
+    public List<Guest> findAllGuest() {
+
+        Session session = HibernateUtils.getSessionFactory().openSession();
+        String hql = "FROM Guest";
+        List<Guest> guests = session.createQuery(hql, Guest.class).getResultList();
+        return guests;
     }
 }
