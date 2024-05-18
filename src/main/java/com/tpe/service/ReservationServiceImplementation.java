a570b78f81e8a656e03212cf6e26e9fafbecd52b
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-18 22:03:27 +0300

    Practice session, Guest Implementations

--- src/main/java/com/tpe/service/ReservationServiceImplementation.java
+++ src/main/java/com/tpe/service/ReservationServiceImplementation.java
@@ -1,10 +1,28 @@
 package com.tpe.service;
 
 import com.tpe.model.Reservation;
+import com.tpe.repository.GuestRepository;
+import com.tpe.repository.ReservationRepository;
+import com.tpe.repository.RoomRepository;
+
+import java.util.Scanner;
 
 public class ReservationServiceImplementation implements ReservationService {
 
-    // step 26 c: save Reservation
+    private Scanner scanner;
+
+    private final ReservationRepository reservationRepository;
+    private final GuestRepository guestRepository;
+    private final RoomRepository roomRepository;
+
+    public ReservationServiceImplementation(ReservationRepository reservationRepository,
+                                            GuestRepository guestRepository, RoomRepository roomRepository) {
+        this.reservationRepository = reservationRepository;
+        this.guestRepository = guestRepository;
+        this.roomRepository = roomRepository;
+    }
+
+    // step 26 d: save Reservation
     @Override
     public Reservation saveReservation() {
         return null;
